ebe42d190b1783b81785429033b97838f451fe55
Satyam Kumar <153222732+Satyambhardwaj59@users.noreply.github.com>
2025-07-05 21:00:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const body = document.querySelector('body');
 buttons.forEach(function (button) {
     // console.log(button);
     button.addEventListener('click', function(e) {
-        // console.log(e);
+        console.log(e);
         // console.log(e.target);
         if(e.target.id === 'grey'){
             body.style.backgroundColor = e.target.id;
